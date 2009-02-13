b831244efb75d862d1ebab8b3df487feef5cb2de
mindu <jmindu@gmail.com>
2009-02-12 22:33:41 -0200

    alterações
    Signed-off-by: mindu <jmindu@gmail.com>

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -3,8 +3,5 @@ class Aluno < ActiveRecord::Base
   SEXO = [['Masculino'], ['Feminino']]
   NIVEL = [['Berçário'],['Maternal I'],['Maternal II'],['Maternal III'], ['Nível I'], ['Nível II'], ['Nível III'], ['Fundamental I'], ['Fundamental II'], ['Fundamental III']]
   PERIODO = [['Parcial'], ['Integral']]
-
-  validates_inclusion_of :sexo, :in => SEXO
-  validates_inclusion_of :nivel, :in => NIVEL
-  validates_inclusion_of :periodo, :in => PERIODO
+  ALIMENTACAO = [['Almoço'], ['Almoço e Janta']]
 end
